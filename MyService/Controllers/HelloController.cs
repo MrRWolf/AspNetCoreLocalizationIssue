b2b9df4c2747b9c29509af56c1364d8ec833aa78
Rick Wolf <rwolf@reliablecontrols.com>
2020-07-30 12:17:47 -0700

    Initial. Not working because missing .fr.resx file

---
+++ MyService/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace MyService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IStringLocalizer<HelloController> local;
+
+        public HelloController(IStringLocalizer<HelloController> localizer)
+        {
+            local = localizer;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return local["standardGreeting"];
+        }
+    }
+}
